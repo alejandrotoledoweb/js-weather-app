a6d92ec72733d1756600759ac8c66f3080e6201b
alejandrotoledoweb <atoledofr@gmail.com>
2021-02-17 16:14:57 -0500

    add dependencies, html and get weather main function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
-import './style/style.css';
-import Icon from './icon.jpg';
+// Dom Elements
 
+const cityName = document.querySelector(".city");
+const weatherCondition = document.querySelector(".weatherCondition");
+const temp = document.querySelector(".temp");
+const feelsLike = document.querySelector(".feelsLike");
+const max = document.querySelector(".tempMax");
+const humidity = document.querySelector(".humidty")
+const deg = document.querySelector(".deg");
+const input = document.querySelector(".searchBar");
+const submit = document.querySelector(".add");
+const slider = document.querySelector(".toggleF")
+const img = document.querySelector(".weatherImage")
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
- // Add the image to our existing div.
- const myIcon = new Image();
- myIcon.src = Icon;
-
- element.appendChild(myIcon);
-
-  return element;
+async function getWeather(location) {
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=metric&appid=d3038b3303b62168dd448fbeb4531d41', {mode: 'cors'})
+  const data = await response.json();
+  const a = data.name;
+  const b = data.main.temp;
+  const c = data.main.feels_like;
+  const d = data.weather[0].description;
+  const e = data.main.humidity;
+  const f = data.main.temp_max;
+  buildPage(a, b, c, d, e, f);
+  getSticker(d);
 }
 
-document.body.appendChild(component());
+getWeather("Toronto");
+
+async function toggleFarenheight() {
+  let location = cityName.textContent
+  let unit = checkState();
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units='+ unit + '&appid=d3038b3303b62168dd448fbeb4531d41', {mode: 'cors'})
+  const data = await response.json();
+  const a = data.name;
+  const b = data.main.temp;
+  const c = data.main.feels_like;
+  const d = data.weather[0].description;
+  const e = data.main.humidity;
+  const f = data.main.temp_max;
+  getSticker(d);
+  buildPage(a, b, c, d, e, f);
+  changeDef();
+};
+
